8e9ea02ae32bf7ede6bde90cc5575abf0bef486f
praburangki <praburangki@gmail.com>
2022-06-28 11:29:26 +0800

    feat(unocss): add dark variant

---
+++ unocss/src/variants/dark.ts
@@ -0,0 +1,18 @@
+import type { Variant } from '@unocss/core';
+import type { PresetVinicuncaOptions } from '../presets';
+
+import { variantMatcher, variantParentMatcher } from '../utils';
+
+export function variantColorsMediaOrClass(options: PresetVinicuncaOptions = {}): Variant[] {
+  if (options?.dark === 'class') {
+    return [
+      variantMatcher('dark', (input) => ({ prefix: `.dark $$ ${input.prefix}` })),
+      variantMatcher('light', (input) => ({ prefix: `.light $$ ${input.prefix}` })),
+    ];
+  }
+
+  return [
+    variantParentMatcher('dark', '@media (prefers-color-scheme: dark)'),
+    variantParentMatcher('light', '@media (prefers-color-scheme: light)'),
+  ];
+}
--- unocss/src/variants/default.ts
+++ unocss/src/variants/default.ts
@@ -1,9 +1,29 @@
 import type { Variant } from '@unocss/core';
 import type { Theme } from '../theme';
-// import type { PresetVinicuncaOptions } from '../presets';
+import type { PresetVinicuncaOptions } from '../presets';
 
 import { variantBreakpoints } from './breakpoints';
+import { variantColorsMediaOrClass } from './dark';
 
-export const variants = (): Variant<Theme>[] => [
+export const variants = (options: PresetVinicuncaOptions): Variant<Theme>[] => [
+  // variantVariables,
+  // variantCssLayer,
+
+  // variantSelector,
+  // variantInternalLayer,
+  // variantNegative,
+  // variantImportant,
+  // variantPrint,
+  // variantCustomMedia,
   variantBreakpoints,
+  // ...variantCombinators,
+
+  // variantPseudoClassesAndElements,
+  // variantPseudoClassFunctions,
+  // ...variantTaggedPseudoClasses(options),
+
+  // partClasses,
+  ...variantColorsMediaOrClass(options),
+  // ...variantLanguageDirections,
+  // variantScope,
 ];
