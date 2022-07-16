e94329ba8dbde30d205e4d290a4d1c09dddc502a
praburangki <praburangki@gmail.com>
2022-07-16 19:23:49 +0800

    draft: prepare component imports

--- packages/vinicunca/component.ts
+++ packages/vinicunca/component.ts
@@ -1,15 +1,114 @@
 import type { Plugin } from 'vue';
 
-// import { VDrawer } from '@vinicunca/components/drawer';
+// --- Base ---
+// import { VButton } from '@vinicunca/components/button';
 import { VIcon } from '@vinicunca/components/icon';
-// import { VInput } from '@vinicunca/components/input';
 import { VScrollbar } from '@vinicunca/components/scrollbar';
+// --- Form ---
+// import { VAutocomplete } from '@vinicunca/components/autocomplete';
+// import { VCascader } from '@vinicunca/components/cascader';
+// import { VCheckbox } from '@vinicunca/components/checkbox';
+// import { VColorPicker } from '@vinicunca/components/color-picker';
+// import { VDatePicker } from '@vinicunca/components/date-picker';
+// import { VDateTimePicker } from '@vinicunca/components/datetime-picker';
+// import { VInput } from '@vinicunca/components/input';
+// import { VRadio } from '@vinicunca/components/radio';
+// import { VRating } from '@vinicunca/components/rating';
+// import { VSelect } from '@vinicunca/components/select';
+// import { VSlider } from '@vinicunca/components/slider';
+// import { VSwitch } from '@vinicunca/components/switch';
+// import { VTimePicker } from '@vinicunca/components/time-picker';
+// import { VUpload } from '@vinicunca/components/upload';
+// --- Data ---
+// import { VAvatar } from '@vinicunca/components/avatar';
+// import { VBadge } from '@vinicunca/components/badge';
+// import { VCalendar } from '@vinicunca/components/calendar';
+// import { VCarousel } from '@vinicunca/components/carousel';
+// import { VChip } from '@vinicunca/components/chip';
+// import { VCollapse } from '@vinicunca/components/collapse';
+// import { VCollapse } from '@vinicunca/components/collapse';
+// import { VEmpty } from '@vinicunca/components/empty';
+// import { VImage } from '@vinicunca/components/image';
+// import { VInfiniteScroll } from '@vinicunca/components/infinitescroll';
+// import { VPagination } from '@vinicunca/components/pagination';
+// import { VProgress } from '@vinicunca/components/progress';
+// import { VSkeleton } from '@vinicunca/components/skeleton';
+// import { VTable } from '@vinicunca/components/table';
+// import { VTree } from '@vinicunca/components/tree';
+// import { VTreeSelect } from '@vinicunca/components/treeselect';
+// --- Navigation ---
+// import { VBacktop } from '@vinicunca/components/backtop';
+// import { VBreadcrumb } from '@vinicunca/components/breadcrumb';
+// import { VSplitter } from '@vinicunca/components/splitter';
+// import { VSteps } from '@vinicunca/components/steps';
+// import { VTabs } from '@vinicunca/components/tabs';
+// --- Feedback ---
+// import { VAlert } from '@vinicunca/components/alert';
+// import { VDialog } from '@vinicunca/components/dialog';
+// import { VDrawer } from '@vinicunca/components/drawer';
+// import { VDialog } from '@vinicunca/components/dialog';
+// import { VLoading } from '@vinicunca/components/loading';
+// import { VMessage } from '@vinicunca/components/message';
+// import { VNotification } from '@vinicunca/components/notification';
+// import { VConfirmation } from '@vinicunca/components/confirmation';
+// import { VPopover } from '@vinicunca/components/popover';
 import { VTooltip } from '@vinicunca/components/tooltip';
 
 export default [
-  // VDrawer,
+  // --- Base ---
+  // VButton,
   VIcon,
-  // VInput,
   VScrollbar,
+
+  // --- Form ---
+  // VAutocomplete,
+  // VCascader,
+  // VCheckbox,
+  // VColorPicker,
+  // VDatePicker,
+  // VDateTimePicker,
+  // VInput,
+  // VRadio,
+  // VRating,
+  // VSelect,
+  // VSlider,
+  // VSwitch,
+  // VTimePicker,
+  // VUpload,
+
+  // --- Data ---
+  // VAvatar,
+  // Badge,
+  // Calendar,
+  // Carousel,
+  // Chip,
+  // Collapse,
+  // Collapse,
+  // Empty,
+  // Image,
+  // InfiniteScroll,
+  // Pagination,
+  // Progress,
+  // Skeleton,
+  // Table,
+  // Tree,
+  // TreeSelect,
+
+  // --- Navigation ---
+  // Backtop,
+  // Breadcrumb,
+  // Splitter,
+  // Steps,
+  // Tabs,
+
+  // --- Feedback ---
+  // VAlert,
+  // VDialog,
+  // VDrawer,
+  // VLoading,
+  // VMessage,
+  // VNotification,
+  // VConfirmation,
+  // VPopover,
   VTooltip,
 ] as Plugin[];
